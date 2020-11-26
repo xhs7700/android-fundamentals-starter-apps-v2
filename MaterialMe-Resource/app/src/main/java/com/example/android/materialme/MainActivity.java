00e7a97278b73d6a115dee802a7457ff6945bac9
xhs7700 <xhs7700@126.com>
2020-11-26 16:39:50 +0800

    update after learning 5.3
    Signed-off-by: xhs7700 <xhs7700@126.com>

--- MaterialMe-Resource/app/src/main/java/com/example/android/materialme/MainActivity.java
+++ MaterialMe-Resource/app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -19,7 +19,7 @@ package com.example.android.materialme;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
@@ -43,11 +43,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
+        int swipeDirs;
+        if (gridColumnCount>1){
+            swipeDirs=0;
+        }else {
+            swipeDirs=ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT;
+        }
+
         // Initialize the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerView);
 
         // Set the Layout Manager.
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, gridColumnCount));
 
         // Initialize the ArrayList that will contain the data.
         mSportsData = new ArrayList<>();
@@ -61,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
         ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
                 ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.DOWN | ItemTouchHelper.UP
-                , ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                , swipeDirs) {
             @Override
             public boolean onMove(RecyclerView recyclerView,
                                   RecyclerView.ViewHolder viewHolder,
